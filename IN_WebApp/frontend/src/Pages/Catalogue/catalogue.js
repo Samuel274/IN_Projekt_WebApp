1387727c0a873848317ef5782f9f2fe8469ace1b
samuel274 <samuel.stein@mailbox.org>
2021-07-29 13:09:58 +0200

    push

---
+++ IN_WebApp/frontend/src/Pages/Catalogue/catalogue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function catalogue() {
+    return (
+        <div>
+            Hier werden alle Module angezeigt
+        </div>
+    )
+}
+
+export default catalogue

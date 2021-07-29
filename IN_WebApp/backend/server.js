280ba72b62d68f1a83ea6bc097b302f547eba848
samuel274 <samuel.stein@mailbox.org>
2021-07-29 15:01:11 +0200

    push

--- IN_WebApp/backend/server.js
+++ IN_WebApp/backend/server.js
@@ -7,6 +7,15 @@ app.use(cors());
 
 const db = require('./models')
 
+// Routers
+
+const usersRouter = require('./routes/Users')
+app.use("/users", usersRouter);
+
+
+
+
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("Server is running on port 3001!");
